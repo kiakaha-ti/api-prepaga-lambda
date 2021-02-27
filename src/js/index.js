90efe23e050f841f25acbad966887d493a5d4488
nacho8 <ignaciovarela8@gmail.com>
2021-02-27 16:25:55 -0300

    refactor: setStrategy

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,11 @@ const StrategyManager = require('./strategy/strategyManager.js');
 const CreateAccountCardStrategy = require('./strategy/createAccountCardStrategy');
 
 exports.handler =  async function(event, context) {
+    const strategyManagerInstance = setStrategies()
     for (let i = 0; i < event.Records.length; i++)
     {
         if (event.Records[i].body) {
             const bodyJson = JSON.parse(event.Records[i].body)
-            const strategyManagerInstance = setStrategies()
             const strategyInstance = strategyManagerInstance.getStrategy(bodyJson.uri)
             strategyInstance.doAction(bodyJson.body, bodyJson.method)
         }
